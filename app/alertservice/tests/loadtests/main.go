2a4ec8f288399bb30d9c11dc3a7c43c7a11e2557
_Knife_ <Kira-Knife@users.noreply.github.com>
2025-02-02 07:07:02 +0300

    Небольшие правки в конфигурацию нагузочных тестов

--- app/alertservice/tests/loadtests/main.go
+++ app/alertservice/tests/loadtests/main.go
@@ -3,8 +3,9 @@ package main
 import "loadtests/testbody"
 
 func main() {
-	numberOfRequests := 100                           // число запросов
-	host := "https://localhost:8787"                  // хост на который пойдут запросы
+	numberOfRequests := 100          // число запросов
+	host := "https://localhost:8787" // хост на который пойдут запросы
+
 	testbody.TestInsertAlerts(host, numberOfRequests) // запросы на вставку данных
 	testbody.TestSelectAlerts(host, numberOfRequests) // запросы на получение данных
 }
